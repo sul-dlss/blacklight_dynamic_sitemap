b2c3bb42bb5c59930e39c13d7e39c8a075d4139e
Jack Reed <phillipjreed@gmail.com>
2020-01-10 14:10:03 -0700

    Add engine default settings

--- lib/blacklight_dynamic_sitemap/engine.rb
+++ lib/blacklight_dynamic_sitemap/engine.rb
@@ -3,5 +3,9 @@
 module BlacklightDynamicSitemap
   class Engine < ::Rails::Engine
     isolate_namespace BlacklightDynamicSitemap
+    config.max_documents_expiration = 1.day
+    config.hashed_id_field = 'hashed_id_ssi'
+    config.unique_id_field = 'id'
+    config.last_modified_field = 'timestamp'
   end
 end
