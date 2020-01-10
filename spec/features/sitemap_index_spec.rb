1e5fac70b5eb32a117e8103edc14f326267be5fa
Jack Reed <phillipjreed@gmail.com>
2020-01-10 14:17:30 -0700

    Update feature specs

--- spec/features/sitemap_index_spec.rb
+++ spec/features/sitemap_index_spec.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-RSpec.describe 'Stiemap Index', type: :feature do
+RSpec.describe 'Sitemap Index', type: :feature do
   it 'renders XML with a root element' do
     visit blacklight_dynamic_sitemap.sitemap_index_path
 
-    expect(page.body).to match('<root/>')
+    expect(page).to have_xpath('//sitemapindex')
+  end
+  it 'renders at least 16 <sitemap> elements' do
+    visit blacklight_dynamic_sitemap.sitemap_index_path
+    expect(page).to have_xpath('//sitemap', count: 16)
   end
 end
--- spec/features/sitemap_show_spec.rb
+++ spec/features/sitemap_show_spec.rb
@@ -1,9 +1,13 @@
 # frozen_string_literal: true
 
-RSpec.describe 'Stiemap Show', type: :feature do
+RSpec.describe 'Sitemap Show', type: :feature do
   it 'renders XML with a root element' do
     visit blacklight_dynamic_sitemap.sitemap_path('1')
 
-    expect(page.body).to match('<root/>')
+    expect(page).to have_xpath('//urlset')
+  end
+  it 'renders <url> elements' do
+    visit blacklight_dynamic_sitemap.sitemap_path('1')
+    expect(page).to have_xpath('//url', count: 6)
   end
 end
