8a8c4bc09b464e4f33120044ecf7db2da8c42623
Jack Reed <phillipjreed@gmail.com>
2020-01-10 08:13:03 -0700

    Setup blacklight to be generated into test app

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -1,14 +1,27 @@
-        require 'rails/generators'
+require 'rails/generators'
 
-        class TestAppGenerator < Rails::Generators::Base
-          source_root "./spec/test_app_templates"
+class TestAppGenerator < Rails::Generators::Base
+  source_root "./spec/test_app_templates"
 
-          # if you need to generate any additional configuration
-          # into the test app, this generator will be run immediately
-          # after setting up the application
+  def add_gems
+    gem 'blacklight', '> 6.0'
 
-          def install_engine
-            generate 'blacklight_dynamic_sitemap:install'
-          end
-        end
+    Bundler.with_clean_env do
+      run 'bundle install'
+    end
+  end
 
+  def run_blacklight_generator
+    say_status('warning', 'GENERATING BL', :yellow)
+
+    generate 'blacklight:install', '--devise'
+  end
+
+  # if you need to generate any additional configuration
+  # into the test app, this generator will be run immediately
+  # after setting up the application
+
+  def install_engine
+    generate 'blacklight_dynamic_sitemap:install'
+  end
+end
