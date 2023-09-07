5a470e5cd4297dac44aa523507c285154b831ea2
Chris Beer <chris@cbeer.info>
2023-09-07 08:07:32 -0700

    Add enough configuration to allow downstream consumers to use the export handler

--- lib/blacklight_dynamic_sitemap/engine.rb
+++ lib/blacklight_dynamic_sitemap/engine.rb
@@ -3,6 +3,14 @@
 module BlacklightDynamicSitemap
   class Engine < ::Rails::Engine
     isolate_namespace BlacklightDynamicSitemap
+    config.default_params = {
+      q: '*:*',
+      rows: 2_000_000, # Ensure that we do not page this result
+      facet: false,
+      defType: 'lucene',
+      sort: 'id asc'
+    }
+    config.solr_endpoint = :select
     config.max_documents_expiration = 1.day
     config.minimum_average_chunk = 10_000
     config.hashed_id_field = 'hashed_id_ssi'
