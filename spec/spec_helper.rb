3497b53545e085651401383a78a96257c6f7cdec
Justin Coyne <jcoyne@justincoyne.com>
2025-04-16 15:07:06 -0500

    Avoid loading blacklight_dynamic_sitemap before the application loads

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,6 @@
-ENV["RAILS_ENV"] ||= 'test'
+ENV['RAILS_ENV'] ||= 'test'
 
-require "bundler/setup"
-require "blacklight_dynamic_sitemap"
+require 'bundler/setup'
 
 require 'engine_cart'
 EngineCart.load_application!
@@ -13,7 +12,7 @@ RSpec.configure do |config|
   config.infer_spec_type_from_file_location!
 
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
